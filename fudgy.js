3d83636c3dd015446cc122843cfecb6690615a7a
NekoCarreraDesigns <denvermaas@gmail.com>
2020-05-11 01:47:18 -0600

    made some style chnages to the home page, added flavors to flavor page, working on purchase function

--- fudgy.js
+++ fudgy.js
@@ -1,4 +1,14 @@
-// var yumms = confirm("do you like chocolate?")
-// if (yumms === true) {
-//     alert("hell yeah!")
-// }
+var yumms = confirm("do you like chocolate?")
+if (yumms === true) {
+    alert("hell yeah!")
+} else { window.location.replace("http://www.theuselessweb.com") }
+
+document.getElementById("buyBtn").addEventListener("click", function () {
+
+    function buyNow() {
+        if (buyBtn === true) {
+            window.location.href("purchase.html")
+        }
+    }
+    buyNow(buyBtn)
+})
