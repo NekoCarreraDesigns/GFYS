22e1212d66809065a1d9e38736be3568ccc32e15
NekoCarreraDesigns <denvermaas@gmail.com>
2020-05-28 22:50:38 -0600

    added nw id's to buttons on flavor page, created extra click functions for flavor page

--- function.js
+++ function.js
@@ -1,9 +1,17 @@
 var buyBtn = document.getElementById("buyBtn")
+var buyBtn1 = document.getElementById("buyBtn1")
+var buyBtn2 = document.getElementById("buyBtn2")
+var buyBtn3 = document.getElementById("buyBtn3")
 
-buyBtn.addEventListener("click", function () {
+buyBtn.addEventListener("click", function (buyBtn) {
     window.location.replace("purchase.html")
-    console.log(buyBtn)
-
-
-
+})
+buyBtn1.addEventListener("click", function (buyBtn) {
+    window.location.replace("purchase.html")
+})
+buyBtn2.addEventListener("click", function (buyBtn) {
+    window.location.replace("purchase.html")
+})
+buyBtn3.addEventListener("click", function (buyBtn) {
+    window.location.replace("purchase.html")
 })
