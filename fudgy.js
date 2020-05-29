4453bc2275ca6b7b386c1f4db5a89ab8796134fb
NekoCarreraDesigns <denvermaas@gmail.com>
2020-05-28 22:25:17 -0600

    removed function from fudgy.js

--- fudgy.js
+++ fudgy.js
@@ -2,14 +2,5 @@ var yumms = confirm("do you like chocolate?")
 if (yumms === true) {
     alert("hell yeah!")
 } else {
-    window.location.assign("http://www.theuselessweb.com")
+    window.location.replace("http://www.theuselessweb.com")
 }
-var buyBtn = document.getElementById("buyBtn")
-
-buyBtn.addEventListener("click", function () {
-    window.location.replace("purchase.html")
-    console.log(buyBtn)
-
-
-
-})
